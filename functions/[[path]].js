b64a8737218ce587634c00ac078b216104d6944a
Oulook12367 <Oulook12367@outlook.com>
2025-06-07 13:30:06 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,4 +1,9 @@
 // functions/[[path]].js
-export async function onRequest(context) {
-  return new Response("Hello, world!");
-}
+import { Hono } from 'hono';
+const app = new Hono();
+
+app.get('/test', (c) => {
+  return c.json({ ok: true, message: 'Hono is working!' });
+});
+
+export const onRequest = app.fetch;
