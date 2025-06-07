302a12f49aaf4871004be5b0faf0921c45d1c5d5
Oulook12367 <Oulook12367@outlook.com>
2025-06-07 13:33:10 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,8 +2,8 @@
 import { Hono } from 'hono';
 const app = new Hono();
 
-app.get('/test', (c) => {
-  return c.json({ ok: true, message: 'Hono is working!' });
+app.get('/test-hono', (c) => {
+  return c.json({ ok: true, message: 'Hono with specific Node.js version is working!' });
 });
 
 export const onRequest = app.fetch;
